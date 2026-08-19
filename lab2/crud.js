772d199040e9b5cd5582bb880e524434d172a756
angel kapasia <angelkapasia@gmail.com>
2026-08-19 10:53:39 +0530

    crud

---
+++ lab2/crud.js
@@ -0,0 +1,56 @@
+import readline from "readline/promises";
+import { stdin, stdout } from "process";
+import {writeFile,readFile} from "fs/promises";
+
+
+const FILE="products.json"
+
+const saveCart=(cart)=>{
+    await writeFile(FILE,JSON.stringify(cart,null,2));
+}
+
+const getCart = () => {
+    const data=await readFile(FILE,"utf-8");
+    return JSON.parse(data);
+
+};
+
+const main = async () => {
+  const cin = readline.createInterface({ input: stdin, output: stdout });
+  let choice;
+  do {
+    console.log("Welcome to shopping cart 🛍️ ");
+    console.log("1   Add to cart");
+    console.log("2   show cart");
+    console.log("3   Remove item");
+    console.log("4   update quantity");
+    console.log("5   checkout");
+    choice = await cin.question("Enter your choice :");
+    console.log("Entered choice:", choice);
+    switch (Number(choice)) {
+      case 1:
+        console.log("add to cart");
+        break;
+      case 2:
+        console.log("show cart items");
+        break;
+
+      case 3:
+        console.log("remove items");
+        break;
+      case 4:
+        console.log("aupdate quantity");
+        break;
+      case 5:
+        console.log("see you later... 😄");
+        process.exit();
+        break;
+      default:
+        console.log("Invalid choice! try again 😕");
+    }
+  } while (choice != 5);
+
+  cin.close();
+};
+
+main();
