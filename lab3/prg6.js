cfa8a0e550da2e5a49e5a93e0a2fe1a321b63b26
angel kapasia <angelkapasia@gmail.com>
2026-09-16 12:04:12 +0530

    add post request with body

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,11 +1,33 @@
-import http from 'http'
-const server = http.createServer((req,res)=>{
-    if(req.url === '/'){
-        res.statusCode = 200;
-        res.end("sent with 200");
-    }else{
-        res.statusCode = 404;
-        res.end("request not found");
-    }
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    res.statusCode = 200;
+    res.end("GET Request");
+  } else if (req.url === "/" && req.method === "POST") {
+    //console.log("Request:", req);
+    let body =''
+    req.on('data',(chunk)=>{
+        body+= chunk;
+    });
+    req.on("end", ()=>{
+        const product = JSON.parse(body);
+        console.log("recieved product:", product);
+          res.statusCode = 201;
+          res.end(JSON.stringify({ msg: "product added", product }));
+    });
+  
+
+  } else if (req.url === "/" && req.method === "PUT") {
+    res.statusCode = 200;
+    res.end("PUT Request");
+  } else if (req.url === "/" && req.method === "DELETE") {
+    res.statusCode = 200;
+    res.end("DELETE Request");
+  } else {
+    res.statusCode = 404;
+    res.end("request not found");
+  }
 });
+
 server.listen(5000, () => console.log("prg6 is running"));
